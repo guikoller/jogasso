d8d3906862f51ed7589105420568976c9700677c
Robson <robson1622@gmail.com>
2021-04-24 18:41:51 -0300

    Menu funcional

--- src/Menu.h
+++ src/Menu.h
@@ -6,10 +6,97 @@
 
 class Menu{
 protected:
-    sf::Text texto;
     sf::Sprite planoFundoSprite;
     sf::Texture selecionarPlano;
     sf::Font selecionarFonte;
+
+    //-------------------------------------------Botoes
+    class Botao{
+        private:
+            sf::RectangleShape retangulo;
+            sf::Text texto;
+            sf::Font localFont;
+            sf::Color corTexto;
+            sf::Color corRetangulo;
+            sf::Color efeito;
+        public:
+            Botao():retangulo(),texto(),localFont(),corTexto(),corRetangulo(),efeito(){}
+            ~Botao(){}
+            Botao(sf::Vector2f tamBotao,const char *textoBotao,sf::Color corText,sf::Color corReta):
+                retangulo(),texto(),localFont(),corTexto(corText),corRetangulo(corReta)
+                {
+                retangulo.setSize(tamBotao);
+                localFont.loadFromFile("font.ttf");
+                retangulo.setFillColor(corRetangulo);
+
+                texto.setFillColor(corTexto);
+                texto.setString(textoBotao);
+                texto.setFont(localFont);
+                texto.setCharacterSize(14);
+            }
+            void botaoPadrao01(const char *text){
+                corRetangulo = sf::Color(79,165,173);
+                corTexto = sf::Color::Black;
+                efeito = sf::Color::White;
+
+                localFont.loadFromFile("font.ttf");
+                retangulo.setSize({200,35});
+                retangulo.setFillColor(corRetangulo);
+
+                texto.setFillColor(corTexto);
+                texto.setString(text);
+                texto.setFont(localFont);
+                texto.setCharacterSize(14);
+            }
+            void setPosicao(sf::Vector2f pos){
+                retangulo.setPosition(pos);
+                float posX = pos.x + ( retangulo.getSize().x / 2 );
+                float posY = pos.y + ( retangulo.getSize().y / 2 );
+                texto.setPosition({posX,posY});
+            }
+            void setCorTexto(sf::Color cor){
+                texto.setFillColor(cor);
+            }
+            void setCorBotao(sf::Color cor){
+                retangulo.setFillColor(cor);
+            }
+            void setTexto(const char *textoBotao){
+                texto.setString(textoBotao);
+            }
+            void setTamBotao(sf::Vector2f tamanho){
+                retangulo.setSize(tamanho);
+            }
+            bool mouseSobreBotao(GerenciadorGrafico *p){
+                float posX = p->getPosicaoMouse().x;
+                float posY = p->getPosicaoMouse().y;
+                
+                float diaSupEsqX = retangulo.getPosition().x;
+                float diaSupEsqY = retangulo.getPosition().y;
+                
+                float diaInfDirX = retangulo.getPosition().x + retangulo.getSize().x;
+                float diaInfDirY  = retangulo.getPosition().y + retangulo.getSize().y;
+
+                if( (posX > diaSupEsqX) && (posY > diaSupEsqY ) && (posX < diaInfDirX ) && (posY < diaInfDirY) ){
+                    retangulo.setFillColor(efeito);
+                    return true;
+                }
+                retangulo.setFillColor(corRetangulo);
+                return false;
+            }
+            void setCorEfeito(sf::Color cor){
+                efeito = cor;
+            }
+            bool pressionado(GerenciadorGrafico *p){
+                if( mouseSobreBotao(p) && p->botaoMousePressionado())
+                    return true;
+                return false;
+            }
+            void desenhar(GerenciadorGrafico *p){
+                p->desenhar(&retangulo);
+                p->desenhar(&texto);
+            }
+    };
+    
 public:
     Menu():planoFundoSprite(),texto(),selecionarPlano(),selecionarFonte(){
         selecionarFonte.loadFromFile("font.ttf");
--- src/MenuPrincipal.cpp
+++ src/MenuPrincipal.cpp
@@ -2,17 +2,44 @@
 #include "GerenciadorGrafico.h"
 
 MenuPrincipal::~MenuPrincipal(){
-
 }
 
 void MenuPrincipal::desenhar(GerenciadorGrafico *p){
-    p->clear();
-    p->desenhaImagem(&planoFundoSprite);
-    p->display();
-    
+    bool select = false;
+    while(!select && p->janelaAberta()){
+        p->clear();
+        p->fecharJanelaPressionado();
+        //p->desenhar(&planoFundoSprite);
+        iniciar.desenhar(p);
+        continuar.desenhar(p);
+        carregar.desenhar(p);
+        rankin.desenhar(p);
+        
+        p->display();
+        if(iniciar.pressionado(p))
+            iniciarJogo(p);
+        if(continuar.pressionado(p))
+            continuarJogo(p);
+        if(rankin.pressionado(p))
+            carregarJogo(p);
+        if(carregar.pressionado(p))
+            ranking(p);
+    }
 }
 
-void MenuPrincipal::ranking(GerenciadorGrafico *p){}
-void MenuPrincipal::carregarJogo(GerenciadorGrafico *p){}
-void MenuPrincipal::continuarJogo(GerenciadorGrafico *p){}
-void MenuPrincipal::iniciarJogo(GerenciadorGrafico *p){}
+void MenuPrincipal::iniciarJogo(GerenciadorGrafico *p){
+    printf("iniciou");
+    p->fechraJanela();
+}
+void MenuPrincipal::continuarJogo(GerenciadorGrafico *p){
+    printf("continuou");
+    desenhar(p);
+}
+void MenuPrincipal::carregarJogo(GerenciadorGrafico *p){
+    printf("carregou");
+    desenhar(p);
+}
+void MenuPrincipal::ranking(GerenciadorGrafico *p){
+    printf("vendo ranking");
+    desenhar(p);
+}
