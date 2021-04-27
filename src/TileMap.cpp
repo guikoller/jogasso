98096b6d1b0e2bd525caf32dcf90c8bcfe22afdf
Guilherme Koller <g_koller@hotmail.com>
2021-04-27 13:48:57 -0300

    bordas do tilemap feitas

--- src/TileMap.cpp
+++ src/TileMap.cpp
@@ -27,10 +27,10 @@ void TileMap::initTiles(){
 	sf::IntRect parede_dir = sf::IntRect(0, 32, 32, 32);
 	sf::IntRect teto = sf::IntRect(32, 64, 32, 32);
 	sf::IntRect chao = sf::IntRect(32, 0, 32, 32);
-	sf::IntRect canto_s_d = sf::IntRect(192, 0, 32, 32);
-	sf::IntRect canto_s_e = sf::IntRect(224, 0, 32, 32);
-	sf::IntRect canto_i_d = sf::IntRect(192, 32, 32, 32);
-	sf::IntRect canto_i_e = sf::IntRect(224, 32, 32, 32);
+	sf::IntRect canto_s_e = sf::IntRect(192, 0, 32, 32);
+	sf::IntRect canto_s_d = sf::IntRect(224, 0, 32, 32);
+	sf::IntRect canto_i_e = sf::IntRect(192, 32, 32, 32);
+	sf::IntRect canto_i_d = sf::IntRect(224, 32, 32, 32);
 	std::vector<Tile *> Linha;
 
 	
@@ -45,19 +45,19 @@ void TileMap::initTiles(){
 		{
 			if (i == 0 && j == 0)//canto superior esquerdo
 			{
-				Linha.push_back(new Tile(this->textura, canto_s_d, i * 64, j * 64,false, true));
-			}
-			else if (i == 0 && (j+1) == this->largura)//canto superior direito
-			{
 				Linha.push_back(new Tile(this->textura, canto_s_e, i * 64, j * 64,false, true));
 			}
-			else if (i == 0 && j == 0)//canto inferior esquerdo
+			else if (i == 0 && (j+1) == this->altura)//canto inferior esquerdo
 			{
-				// Linha.push_back(new Tile(this->textura, canto_s_d, i * 64, j * 64,false, true));
+				Linha.push_back(new Tile(this->textura, canto_i_e, i * 64, j * 64,false, true));
 			}
-			else if (i == 0 && j == 0)//canto inferior direito
+			else if (j == 0 && (i+1) ==this->largura)//canto inferior esquerdo
 			{
-				// Linha.push_back(new Tile(this->textura, tijolo, i * 64, j * 64,false, true));
+				Linha.push_back(new Tile(this->textura, canto_s_d, i * 64, j * 64,false, true));
+			}
+			else if ((i+1) == this->largura && (j+1) == this->altura )//canto inferior direito
+			{
+				Linha.push_back(new Tile(this->textura, canto_i_d, i * 64, j * 64,false, true));
 			}
 			else if (i == 0)//parede esquerda
 			{
