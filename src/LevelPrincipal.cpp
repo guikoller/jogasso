4459ff4dc419a0db832142bc9d097792a9ca9598
Guilherme Koller <g_koller@hotmail.com>
2021-05-04 11:13:20 -0300

    levelPrincipal para LevelPrincipal

---
+++ src/LevelPrincipal.cpp
@@ -0,0 +1,119 @@
+#include "LevelPrincipal.h"
+
+void LevelPrincipal::initEntidade(){
+    this->espadachim = new Espadachim();
+    this->mapa = new MapaPrincipal();
+
+    this->espadachim->hitBox.setPosition(sf::Vector2f(200.f,200.f));
+}
+
+LevelPrincipal::LevelPrincipal(/* args */){
+    initEntidade();
+}
+
+LevelPrincipal::~LevelPrincipal(){
+    delete this->espadachim;
+    delete this->mapa;
+}
+
+void LevelPrincipal::updateColisao(){
+
+    sf::FloatRect nextPos;
+
+    for (int i = 0; i < this->mapa->getLargura(); i++)
+    {
+        for (int j = 0; j < this->mapa->getAltura(); j++)
+        {
+            sf::FloatRect playerBounds = espadachim->getGlobalBounds();
+            sf::FloatRect tileBounds = this->mapa->getSprite(i,j).getGlobalBounds();
+            
+            nextPos = playerBounds;
+            nextPos.left += this->espadachim->velocidade.x;
+            nextPos.top += this->espadachim->velocidade.y;
+            
+
+
+            // (x, y)-----------------------(x + width, y)
+            // |                                         |
+            // |                                         |
+            // |                                         |
+            // |                                         |
+            // |                                         |
+            // (x, y + height)-----(x + width, y + height)
+           
+            if (this->mapa->getSolido(i,j))
+            {
+                if (tileBounds.intersects(nextPos))
+                {
+                    //baixo
+                    if (playerBounds.top < tileBounds.top
+                        && playerBounds.top + playerBounds.height < tileBounds.top + tileBounds.height
+                        && playerBounds.left < tileBounds.left + tileBounds.width
+                        && playerBounds.left + playerBounds.width > tileBounds.left
+                    )
+                    {
+                        this->espadachim->velocidade.y = 0.f;
+                        this->espadachim->velocidade.x = 0.f;
+                        this->espadachim->hitBox.setPosition(espadachim->hitBox.getPosition().x, tileBounds.top - playerBounds.height);
+                        printf("colisão chão\n"); 
+                    }
+                    // cima
+                    else if (playerBounds.top > tileBounds.top
+                        && playerBounds.top + playerBounds.height > tileBounds.top + tileBounds.height
+                        && playerBounds.left < tileBounds.left + tileBounds.width
+                        && playerBounds.left + playerBounds.width > tileBounds.left
+                    )
+                    {
+                        this->espadachim->velocidade.y = 0.f;
+                        // this->espadachim->setPosicao(espadachim->hitBox.getPosition().x, tileBounds.top + tileBounds.height + 20);
+                        printf("colisão topo\n"); 
+                    } 
+                    //direita
+                    else if (playerBounds.left < tileBounds.left
+                        && playerBounds.left + playerBounds.width < tileBounds.left + tileBounds.width
+                        && playerBounds.top < tileBounds.top + tileBounds.height
+                        && playerBounds.top + playerBounds.height > tileBounds.top
+                    )
+                    {
+                        this->espadachim->velocidade.x = 0.f;
+                        // this->espadachim->setPosicao(this->espadachim->getPosicao().x+ playerBounds.width, playerBounds.top); 
+                        printf("colisão direita\n"); 
+
+                    }
+                    //esquerda
+                    else if (playerBounds.left > tileBounds.left
+                        && playerBounds.left + playerBounds.width > tileBounds.left + tileBounds.width
+                        && playerBounds.top < tileBounds.top + tileBounds.height
+                        && playerBounds.top + playerBounds.height > tileBounds.top
+                    )
+                    {
+                        this->espadachim->velocidade.x = 0.f;
+                        // this->espadachim->setPosicao(this->espadachim->getPosicao().x,this->espadachim->getPosicao().y);
+                        printf("colisão esquerda\n"); 
+                    }    
+                }
+                
+            }            
+        }
+        
+    }
+
+
+
+
+}
+
+void LevelPrincipal::updateEntidade(){
+    this->espadachim->update();
+}
+
+void LevelPrincipal::update(){
+    updateEntidade();
+    updateColisao();
+}
+
+void LevelPrincipal::render(sf::RenderTarget&target){
+    // printf("renderizado\n");
+    this->mapa->render(target);
+    this->espadachim->render(target);
+}
