0c99fea93579e58a903f564b5d7f2bbd13de74d1
Fischer28 <leo.fischer15@hotmail.com>
2021-05-08 18:50:03 -0300

    Colisao funcionando

--- src/LevelPrincipal.cpp
+++ src/LevelPrincipal.cpp
@@ -27,16 +27,84 @@ LevelPrincipal::~LevelPrincipal(){
 }
 
 void LevelPrincipal::updateColisao(){
+     sf::FloatRect nextPos;
+
     for (int i = 0; i < this->mapa->getLargura(); i++)
     {
         for (int j = 0; j < this->mapa->getAltura(); j++)
         {
-            if(Collision::PixelPerfectTest(this->espadachim->hitBox, this->mapa->getSprite(i,j)) && this->mapa->getSolido(i,j)){
-                this->espadachim->resetVelY();
-                this->espadachim->resetVelX();
-                printf("COLIDIU\n");
-            }   
+            sf::FloatRect playerBounds = espadachim->getGlobalBounds();
+            sf::FloatRect tileBounds = this->mapa->getSprite(i,j).getGlobalBounds();
+            
+            nextPos = playerBounds;
+            nextPos.left += this->espadachim->velocidade.x;
+            nextPos.top += this->espadachim->velocidade.y;
+            
+
+
+            // (x, y)-----------------------(x + width, y)
+            // |                                         |
+            // |                                         |
+            // |                                         |
+            // |                                         |
+            // |                                         |
+            // (x, y + height)-----(x + width, y + height)
+           
+            if (this->mapa->getSolido(i,j))
+            {
+                if (tileBounds.intersects(nextPos))
+                {
+                    //baixo
+                    if (playerBounds.top < tileBounds.top
+                        && playerBounds.top + playerBounds.height < tileBounds.top + tileBounds.height
+                        && playerBounds.left < tileBounds.left + tileBounds.width
+                        && playerBounds.left + playerBounds.width > tileBounds.left
+                    )
+                    {
+                        this->espadachim->velocidade.y = 0.f;
+                        this->espadachim->velocidade.x = 0.f;
+                        this->espadachim->hitBox.setPosition(espadachim->hitBox.getPosition().x, tileBounds.top - playerBounds.height - 10);
+                        printf("colisão chão\n"); 
+                    }
+                    // cima
+                    if (playerBounds.top > tileBounds.top
+                        && playerBounds.top + playerBounds.height > tileBounds.top + tileBounds.height
+                        && playerBounds.left < tileBounds.left + tileBounds.width
+                        && playerBounds.left + playerBounds.width > tileBounds.left
+                    )
+                    {
+                        this->espadachim->velocidade.y = 0.f;
+                        this->espadachim->setPosicao(espadachim->hitBox.getPosition().x, tileBounds.top + tileBounds.height + 10);
+                        printf("colisão topo\n"); 
+                    } 
+                    //direita
+                    else if (playerBounds.left < tileBounds.left
+                        && playerBounds.left + playerBounds.width < tileBounds.left + tileBounds.width
+                        && playerBounds.top < tileBounds.top + tileBounds.height
+                        && playerBounds.top + playerBounds.height > tileBounds.top
+                    )
+                    {
+                        this->espadachim->velocidade.x = 0.f;
+                        this->espadachim->setPosicao(this->espadachim->getPosicao().x - playerBounds.width + 25, playerBounds.top); 
+                        printf("colisão direita\n"); 
+
+                    }
+                    //esquerda
+                    else if (playerBounds.left > tileBounds.left
+                        && playerBounds.left + playerBounds.width > tileBounds.left + tileBounds.width
+                        && playerBounds.top < tileBounds.top + tileBounds.height
+                        && playerBounds.top + playerBounds.height > tileBounds.top
+                    )
+                    {
+                        this->espadachim->velocidade.x = 0.f;
+                        this->espadachim->setPosicao(this->espadachim->getPosicao().x + playerBounds.width - 25,this->espadachim->getPosicao().y);
+                        printf("colisão esquerda\n"); 
+                    }    
+                }
+                
+            }            
         }
+        
     }
 }
 
