d2e9946c7865dd059860e748fb4a9ccae573eaa8
Guilherme Koller <g_koller@hotmail.com>
2021-05-09 16:11:13 -0300

    comando pro fischer

--- src/MapaFinal.cpp
+++ src/MapaFinal.cpp
@@ -39,11 +39,11 @@ void MapaFinal::desenhaMapa(){
 		for (int j = 0; j < this->altura; j++)
 		{
 			
-			if (i == 0 && j == 0)//canto superior esquerdo
+			if (i == 2 && j == 3)//canto superior esquerdo
 			{
 				Linha.push_back(new Tile(this->textura, canto_s_e, i * 64, j * 64,false, true));
 			}
-			else if (i == 0 && (j+1) == this->altura)//canto inferior esquerdo
+			else if (i == 4 && j == 4)//canto inferior esquerdo
 			{
 				Linha.push_back(new Tile(this->textura, canto_i_e, i * 64, j * 64,false, true));
 			}
@@ -55,23 +55,23 @@ void MapaFinal::desenhaMapa(){
 			{
 				Linha.push_back(new Tile(this->textura, canto_i_d, i * 64, j * 64,false, true));
 			}
-			else if (i == 0)//parede esquerda
+			else if (i == 2 && j >= 4)//parede esquerda
 			{
 				Linha.push_back(new Tile(this->textura, parede_esq, i * 64, j * 64,false, true));
 			}
-			else if (j== 0)//teto
+			else if (j== 3)//teto
 			{
 				Linha.push_back(new Tile(this->textura, teto, i * 64, j * 64,false, true));
 			}
-			else if (j+1 == this->altura)//chão
+			else if (j+1 == this->altura-2)//chão
 			{
 				Linha.push_back(new Tile(this->textura, chao, i * 64, j * 64,false, true));
 			}
-			else if (i+1 == this->largura)//parede direita
+			else if (i+1 == this->largura-2)//parede direita
 			{
 				Linha.push_back(new Tile(this->textura, parede_dir, i * 64, j * 64,false, true));
 			}
-			else if ((i > 0 && i < 20 && j == 10)|| (i > 5 && i < 25 && j == 5))//chão 2
+			else if (i > 0 && i < 20 && j == 10)//chão 2
 			{
 				Linha.push_back(new Tile(this->textura, chao2, i * 64, j * 64,false, true));
 			}
